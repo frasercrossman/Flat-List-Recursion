4820bc00f73b55e525d9c44757db97b89f686d55
frasercrossman <frasercrossman@outlook.com>
2016-10-27 21:46:55 +0100

    Inserted comments to assoc-all
    These comments are necessary as the module 'Submission Format' guidelines
    require it.
    It also provides a detailed explanation as the the justification for the
    use of "equal?" as opposed to "eq?" or "eqv?" which all provide similar,
    but distinct, functionality.

--- solutions/assoc-all.scm
+++ solutions/assoc-all.scm
@@ -1,3 +1,10 @@
+;;; Coursework 01
+;;; Question 01
+;;; Fraser Crossman (fc4g15@soton.ac.uk)
+;;;
+
+;;; Solution:
+
 (define assoc-all
   (lambda (sym alist)
     (if (null? alist)
@@ -5,3 +12,29 @@
         (if (equal? sym (caar alist))
             (cons (cdar alist) (assoc-all sym (cdr alist)))
             (assoc-all sym (cdr alist))))))
+
+;;; Solution Comments:
+;;; To construct the list of values associated with the specified 
+;;; symbolic expression in the specified association list the following 
+;;; method was used.
+;;; 
+;;; Recursively traverse the association list, inspecting the value of 
+;;; the key of the first association in the list on each call.
+;;; If the predicate "(equal? <symbolic-exp> <key>)" is satisfied then 
+;;; construct a pair with the car pointing to the associated value of 
+;;; the matching key, and the cdr pointing to the recursively determined 
+;;; list of other values associated with a matching key.
+;;;
+;;; The use of "equal?" as opposed to "eq?" or "eqv?" is important.
+;;; "eq?"		- Returns true if two values share the same enivronment 
+;;;						pointer otherwise returns false.
+;;; "eqv?"	- Returns true if two atomic values are equal or as "eq?" 
+;;;						returns true otherwise returns false.
+;;; "equal?"- Returns true if two lists, vectors, etc. have corresponding 
+;;;						elements or as "eqv?" returns true otherwise returns false.
+;;;						
+;;; The equivalent 'corresponding elements' feature is important as the 
+;;; key may not be an atomic value and so when checked for equivalence 
+;;; with a symbolic expression all corresponding elements must be checked 
+;;; for equivalence.
+;;;
