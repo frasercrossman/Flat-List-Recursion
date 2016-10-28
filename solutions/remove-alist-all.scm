b354aba9fff80a46697fc3e4f0b8c711f548a473
frasercrossman <frasercrossman@outlook.com>
2016-10-28 14:16:34 +0100

    Fixed calls to old function name

--- solutions/remove-alist-all.scm
+++ solutions/remove-alist-all.scm
@@ -1,8 +1,23 @@
+;;; Coursework 01
+;;; Question 02
+;;; Fraser Crossman (fc4g15@soton.ac.uk)
+;;;
+
+;;; Solution:
+
 (define remove-alist-all
   (lambda (sym alist)
     (if (null? alist)
         '()
         (if (equal? sym (caar alist))
-            (remove-assoc-all sym (cdr alist))
-            (cons (car alist) (remove-assoc-all sym (cdr alist)))))))
-            
+            (remove-alist-all sym (cdr alist))
+            (cons (car alist) (remove-alist-all sym (cdr alist)))))))
+
+;;; Solution Comments:
+;;; To construct the association list of associations excluding those with 
+;;; keys equivalent to the specified symbolic expression in the specified 
+;;; association list the following method was used.
+;;; 
+;;; Recursively traverse the association list, inspecting the value of 
+;;; the key of the first association in the list on each call.
+;;;            
